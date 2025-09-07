5608851f1e4088ad71abaeeaa7e2cf460f276982
LiHuarong <168278282@qq.com>
2025-09-07 17:16:45 +0800

    分享图片的地址增加查看客户当天其他的快递记录，增加是否中奖字段

---
+++ php/getCjData.php
@@ -0,0 +1,87 @@
+<?php
+
+// 获取抽奖结果（传入一个快递的id，然后返回这个id对应的这个人今天除了id以外的所有快递）
+
+include 'db.php';
+include 'lib.php';
+
+$pid = $_POST['pid'];
+
+if (checkParm($pid)) {
+    $sql = "SELECT building_users_id FROM `data` WHERE id = '$pid'";
+    $result = $conn->query($sql);
+    if ($result->num_rows > 0) {
+        $data = $result->fetch_assoc();
+        $data = $data['building_users_id'];
+        $sql = "SELECT * FROM `data` WHERE building_users_id = '$data' AND id != '$pid' AND `time` = CURDATE()";
+        $result = $conn->query($sql);
+        if ($result->num_rows > 0) {
+            $data = $result->fetch_all(MYSQLI_ASSOC);
+            $new_data = array();
+            for ($i = 0; $i < count($data); $i++) {
+                $status = $data[$i]['status'];
+                $id = $data[$i]['id'];
+                if ($status == "未送达") {
+                    $status = 0;
+                } else if ($status == "待分享") {
+                    $status = 1;
+                } else if ($status == "已完成") {
+                    $status = 2;
+                } else {
+                    $status = 0;
+                }
+
+                // 中奖状态
+                $cjStatus;
+                $todayCjDetail = getTodayCJ($conn);
+                if ($todayCjDetail != "false") {
+                    // $todayCjDetail = json_decode($todayCjDetail, true);
+                    $y_n = $todayCjDetail['y_n'];
+                    if ($y_n == "false") {
+                        $cjStatus = 4;//没开奖
+                    } else {
+                        $cj = getCJdetail($id, $conn);//获取抽奖状态
+                        if ($cj) {
+                            $cjStatus = 2;//中奖
+                        } else {
+                            $cjStatus = 3;//未中奖
+                        }
+                    }
+                } else {
+                    $cjStatus = 1;//没有抽奖活动
+                }
+
+                $new_data[$i] = array(
+                    "expressNo" => $data[$i]['id'],
+                    "deliveryStatus" => $status,
+                    "lotteryStatus" => $cjStatus
+                );
+            }
+            $arr = array(
+                "code" => 200,
+                "msg" => "成功",
+                "data" => $new_data
+            );
+        } else {
+            $arr = array(
+                "code" => 201,
+                "msg" => "今天只有一个快递",
+                "data" => []
+            );
+        }
+    } else {
+        $arr = array(
+            "code" => 404,
+            "msg" => "没有找到这个快递",
+            "data" => []
+        );
+    }
+} else {
+    $arr = array(
+        "code" => 401,
+        "msg" => "参数错误",
+        "data" => []
+    );
+}
+
+echo json_encode($arr);
--- php/getImgDetail.php
+++ php/getImgDetail.php
@@ -3,6 +3,7 @@
 // 获取图片分享链接
 
 include 'db.php';
+include 'lib.php';
 
 $pid = $_GET['pid'];
 
@@ -14,6 +15,23 @@ if ($result->num_rows > 0) {
     $data = array();
     while ($row = $result->fetch_assoc()) {
         $data[] = $row;
+    }
+    $todayCjDetail = getTodayCJ($conn);
+    if ($todayCjDetail != "false") {
+        // $todayCjDetail = json_decode($todayCjDetail, true);
+        $y_n = $todayCjDetail['y_n'];
+        if ($y_n == "false") {
+            $data[0]['cj'] = "4";//没开奖
+        } else {
+            $cj = getCJdetail($pid, $conn);//获取抽奖状态
+            if ($cj) {
+                $data[0]['cj'] = "2";//中奖
+            } else {
+                $data[0]['cj'] = "3";//未中奖
+            }
+        }
+    } else {
+        $data[0]['cj'] = "1";//没有抽奖活动
     }
     $arr = array(
         "code" => 200,
