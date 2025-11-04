680c922005331f98dae1a9fd271f2265be508c26
LiHuarong <168278282@qq.com>
2025-11-04 09:55:10 +0800

    增加log.js，挂载到全局top，记录用户日志

---
+++ js/log.js
@@ -0,0 +1,19 @@
+// 记录日志
+
+// !!!!!!!必须首先加载lib.js，因为要引用其全局$HOST变量
+
+
+// create by LiHuarong at 2025-11-4
+
+
+// fuck!
+top.pushLog = function (url, detail) {
+    console.log("触发日志记录");
+
+    let xhr = new XMLHttpRequest();
+    xhr.open("POST", $HOST + "/log", true);
+    // 表单数据
+    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    // 不接收数据返回，不考虑是否记录成功
+    xhr.send(`url=${encodeURIComponent(url)}&detail=${encodeURIComponent(detail)}`);
+}
